28efd9bc5009e898362f977007e6a4777e20d768
Elise Phan <aliceconnic@gmail.com>
2019-01-24 08:26:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,19 +23,5 @@ class App < Sinatra::Base
 
     erb :team
   end
-  post '/teams' do
-    @team_name = params[:team][:name]
-    @team_motto = params[:team][:motto]
-    @hero_name = []
-    @hero_power = []
-    @hero_bio = []
-    @team_members = params[:team][:members]
-    @team_members.each do |hero|
-      @hero_name << hero[:name]
-      @hero_power << hero[:power]
-      @hero_bio << hero[:bio]
-    end
 
-    erb :team
-  end
 end
